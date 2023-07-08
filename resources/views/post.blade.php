6a0851d73238276ac237c608b3a1d685432f78f1
lk989 <lma.bugis@hotmail.com>
2023-07-08 17:14:08 +0300

    test

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -10,7 +10,13 @@
 
 <body>
     <article>
-        <?= $post; ?>    
+        <h1>
+            <?= $post->title; ?>
+        </h1>
+
+        <div>
+            <?= $post->body; ?>
+        </div>
     </article>
 
     <a href="/">Go back</a>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Models\Post;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,20 +18,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('posts');
+    return view('posts', [
+        'posts' => Post::all()
+    ]);
 });
 
 Route::get('posts/{post}', function($slug){
-    $path = __DIR__ . "/../resources/posts/{$slug}.html";
-
-    if(! file_exists($path)){
-        return redirect('/');
-        // abort(404);
-    }
-
-    $post = file_get_contents($path);
-
     return view('post', [
-        'post' => $post
+        'post' => Post::find($slug)
     ]);
-});
+
+}) -> where('post', '[A-z_\-]+');
