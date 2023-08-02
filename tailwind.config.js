52da38db653c181ace1b9cdf4d13669300ab2a0c
Lama <lma.bugis@hotmail.com>
2023-08-02 16:32:47 +0300

    binding resolution

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,21 +7,6 @@ export default {
     ],
     theme: {
         extend: {},
-        colors: {
-            navy: "#243c4c",
-            "zinc-200": "#e4e4e7",
-            "zinc-400": "#a1a1aa",
-            brown: "#836f52",
-            gold: "#c3ad77",
-            navy2: "#164e63",
-            zinc: "#fafafa",
-
-            wine: "#771534",
-            darkgray: "#3e3e3f",
-            lightgray: "#f2f1f1",
-            pink: "#b07889",
-            yellow: "#eeb13b",
-        },
     },
     plugins: [],
 };
